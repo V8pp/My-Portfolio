3ef0e72b24d414cfba87dca3f3bf81c68944d559
V8pp <vedp1053@gmail.com>
2025-07-23 18:51:34 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBHqj_Gjdr6J3PhXcu_sEKXgYyvlAp1SXo",
-  authDomain: "ved-portf.firebaseapp.com",
-  projectId: "ved-portf",
-  storageBucket: "ved-portf.firebasestorage.app",
-  messagingSenderId: "737814139525",
-  appId: "1:737814139525:web:2252b16b4213cfe14ecfec"
-};
